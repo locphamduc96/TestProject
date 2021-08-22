61465f70f252835d1788d7a35da1b24e398917e1
locphamduc96 <locphamduc96@gmail.com>
2021-08-22 20:52:17 +0700

    initial commit

---
+++ src/main/java/org/yourcompany/yourproject/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author locpd2
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.err.println("Hello World");
+
+        System.err.println("Hello World");
+
+        System.exit(0);
+    }
+
+}
